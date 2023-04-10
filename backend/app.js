78927963ca1af4e3d37a5b07c9390c7df4dee214
Abdullah <abdullahshafi776@gmail.com>
2023-04-11 00:04:29 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -29,17 +29,17 @@ if ((process.env.NODE_ENV || "").trim() !== "production") {
   })
 }
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-//   app.get("*", (req, res) =>
-//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("Server is running....");
-//   });
-// }
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("Server is running....");
+  });
+}
 
 app.use(notFound)
 app.use(errorHandler)
