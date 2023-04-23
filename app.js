87a84061156ada900d400908754c548cad4ef5d1
Abdullah <abdullahshafi776@gmail.com>
2023-04-23 22:35:24 +0500

    main

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 import express from "express"
 import path from "path"
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 import productRoutes from "./routes/productRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
 import orderRoutes from "./routes/orderRoutes.js"
 import uploadRoutes from "./routes/uploadRoutes.js"
 
-const app = express()
+dotenv.config();
+connectDB();
 
+const app = express()
 app.use(express.json())
+
 app.use("/api/products", productRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
@@ -17,31 +22,34 @@ app.use("/api/uploads", uploadRoutes)
 const __dirname = path.resolve()
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
 
-if ((process.env.NODE_ENV || "").trim() !== "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")))
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  )
-} else {
-  app.get("/", (req, res) => {
-    res.send("Server is running.")
-  })
-}
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/build")));
+// if ((process.env.NODE_ENV || "").trim() !== "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")))
 
 //   app.get("*", (req, res) =>
 //     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-//   );
+//   )
 // } else {
 //   app.get("/", (req, res) => {
-//     res.send("Server is running...");
-//   });
+//     res.send("Server is running.")
+//   })
 // }
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("Server is running...");
+  });
+}
 
 app.use(notFound)
 app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running on Port:${PORT}`));
 
 export default app
